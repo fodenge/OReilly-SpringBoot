97020d9d7c3b3294f5a9020a99e22d6e1c836ac4
Shruti <shrutipandeyofficial@gmail.com>
2025-02-10 14:33:44 +0530

    Develop Course API

---
+++ learn-spring-boot/src/main/java/com/oreilly/springboot/learn_spring_boot/CourseController.java
@@ -0,0 +1,18 @@
+package com.oreilly.springboot.learn_spring_boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1,"Learn Spring Boot","O'Reilly"),
+                new Course(2,"Learn AWS","Percipio")
+        );
+    }
+}
